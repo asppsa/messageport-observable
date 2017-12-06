f25839e3efd0832e2a3742eef7cb59151c132af3
Alastair Pharo <asppsa@gmail.com>
2017-12-07 10:19:27 +1300

    Update examples

--- examples/increment.js
+++ examples/increment.js
@@ -2,10 +2,19 @@ importScripts('index.js');
 
 let x = 1;
 
+let wrapPort = MessagePortObservable.wrapPort;
+
 self.onconnect = function(connectEvent) {
-  let port = MessagePortObservable.wrapPort(connectEvent.ports[0]);
-  port.subscribeAndPostReplies(event => {
-    x += 1;
-    return [x];
-  });
+  let port = wrapPort(connectEvent.ports[0]);
+  port.filter(event => event.data === 'inc')
+    .subscribeAndPostReplies(event => {
+      x += 1;
+      return [`inc ${x}`];
+    });
+
+  port.filter(event => event.data === 'dec')
+    .subscribeAndPostReplies(event => {
+      x -= 1;
+      return [`dec ${x}`];
+    });
 };
--- examples/worker.js
+++ examples/worker.js
@@ -1,11 +1,16 @@
 importScripts('index.js');
 
-let myPort = MessagePortObservable.wrapPort(self);
-myPort.subscribeAndPostReplies(event => {
-  switch(event.data) {
-    case 'ping':
-      console.log('ping');
-      return ['pong'];
-      break;
-  }
-});
+const wrapPort = MessagePortObservable.wrapPort;
+
+const myPort = wrapPort(self);
+myPort.filter(event => event.data === 'ping')
+  .subscribeAndPostReplies(event => {
+    console.log('ping', event.data);
+    return ['pong'];
+  });
+
+myPort.filter(event => event.data === 'pong')
+  .subscribeAndPostReplies(event => {
+    console.log('pong', event.data);
+    return ['ping'];
+  });
